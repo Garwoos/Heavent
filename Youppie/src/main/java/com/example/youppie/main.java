d982a87271206e71f78c47aa555f2275ae674ca8
Garwoos <saminochko@gmail.com>
2024-02-08 14:55:49 +0100

    a

---
+++ Youppie/src/main/java/com/example/youppie/main.java
@@ -0,0 +1,29 @@
+package com.example.youppie;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Youppie Tralala !");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+}
