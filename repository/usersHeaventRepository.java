10026bddcc05082bec8a84e5c8e2cad5c9b827e0
Garwoos <115900098+Garwoos@users.noreply.github.com>
2024-03-29 19:48:52 +0100

    Delete repository directory

--- repository/usersHeaventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Heavent.Heavent.repository;
-
-import com.Heavent.Heavent.modele.usersHeavent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface usersHeaventRepository  extends JpaRepository<usersHeavent , String> {
-
-}
